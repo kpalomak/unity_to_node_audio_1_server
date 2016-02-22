6e25afc339fe9404a8af5810b791b5049677dceb
Reima Karhila <reima.karhila@aalto.fi>
2016-02-22 18:13:35 +0200

    Recognition works!

--- recogniser_client.js
+++ recogniser_client.js
@@ -10,12 +10,25 @@ var client = null;
 
 var connected = false;
 
+var endtag='\x0d\x0a';
+
+var state = 'init';
+
+var audio_queue = [];
+
+var sending_data = false;
+
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+
 function recogniser_client () {
     var dummy  = 1;
 }
 
 
-function init(conf) {
+function init(conf, res, initcallback) {
     var net = require('net');
 
     var HOST = '127.0.0.1';
@@ -23,40 +36,77 @@ function init(conf) {
     
     recog_conf = conf;
 
+
     client = new net.Socket();
     client.connect(PORT, HOST, function() {	
 	// console.log('CONNECTED TO: ' + HOST + ':' + PORT);
 	// Write a message to the socket as soon as the client is connected:
 	connected = true;
-
-	define_grammar(client, recog_conf.grammar);
+	define_speaker('foo');
 
     });
     
     // Add a 'data' event handler for the client socket
     // data is what the server sent to this socket
+
+
     client.on('data', function(data) {
-	if (data.toString().substr(0,25) == 'INFO: Server was started.') {
-	    console.log('¤¤¤¤ Recogniser server happily started:');
+	//console.log("Got data! >>>"+data+"<<<");
+
+
+	if (data.toString().substr(0,23) == '0000000000000000027 200') {
+
+	    //console.log('recogniser says OK (my state is '+state+')');
+	    
+	    if (state == 'init') {
+		console.log(' --> grammar_def');		
+		state = 'grammar-def';
+		define_grammar(recog_conf.grammar);
+	    }
+	    else if (state == 'grammar-def') {
+
+		//state = 'word-def';
+		//define_word("choose");
+
+		console.log(' --> start_recog');
+		state = 'start_recog';
+		start_recog();
+	    }
+	    else if (state == 'word-def') {
+		console.log(' --> let\s go segment!');
+		state = 'segment';
+		initcallback(res);
+	    }
+	    else if (state == 'start_recog') {
+		console.log(' --> let\'s go recognise!');
+		state = 'recognise';
+		initcallback(res);
+	    }
 	}
-	else if (data.toString().substr(0,14) == 'DEFINE-GRAMMAR') {
-	    console.log('¤¤¤¤ Reply from grammar:');
-	    console.log(data.toString());
-	    console.log('¤¤¤¤/reply from grammar:');
+	else if (data.toString().indexOf('RECOGNITION-COMPLETE') > -1) {
+	    state = 'got_result';
+	    arr =  data.toString().split("\n");
+	    if (arr[1].substr(0,20) == 'Completion-Cause:000') {
+		word = arr[4];
+		console.log("### Recognised \""+word+"\" ###");
+	    }
+	    else {
+		console.log("Something went wrong: \n"+data);
+	    }
+	    reset_recogniser();
 	}
-	else if (data.toString().substr(0,21) == 'SEGMENTATION-COMPLETE') {
-	    console.log('¤¤¤¤ Reply from segmentation:');
-	    console.log(data.toString());
-	    console.log('¤¤¤¤/reply from segmentation:');	    
-	}
-	else {
-	    console.log("¤¤¤¤ RESPONSE: "+data.toString());
-	    console.log("¤¤¤¤/RESPONSE");
-	}
+	else { console.log("Does not fit this: "+data);} 
+
+
+
 	// Close the client socket completely
 	//client.destroy();	
     });
     
+    client.on('error', function (err) {
+	console.log(err.stack);
+    });
+
     // Add a 'close' event handler for the client socket
     client.on('close', function() {
 	console.log('Connection closed');
@@ -65,29 +115,37 @@ function init(conf) {
 }
 
 
-var endtag='\x0d\x0a';
+
 
 function mrcp_message(command, headers, data) {
-
+    
     var msg=' ' + command + endtag;
 
     headers.forEach( function (item) {
 	msg += item.name +":"+item.value+endtag;
     });
     msg += endtag;
-
-    msg += data;
     
-    msgpad='';
-    for (var n =(''+(msg.length+10)).length; n > 0; n--){
+    if (data) {
+	msglength=msg.length+data.length+10;
+    } 
+    else {
+	msglength=msg.length+10;
+    }
+    
+    msgpad=0;
+    for (var n =9-(msglength.toString().length); n > 0; n--){
 	msgpad += '0';
     }
+    
+    
+    msg=msgpad+(msglength) + msg;
+    //console.log("========== msg to recogniser ==========");
+    //console.log(msg);
+    //console.log("==========/msg to recogniser ==========");
+    //console.log("msg to recog: >"+msg+"<");
 
-    msg=msgpad+(msg.length+10)+msg;
-    console.log("========== msg to recogniser ==========");
-    console.log(msg.substr(0, Math.min(msg.length, 50)));
-    console.log("==========/msg to recogniser ==========");
-
+    
     return msg;
 }
 
@@ -112,13 +170,14 @@ function define_grammar(grammar) {
 
 
 function define_word(word) {
+    console.log("===================================> Trying to define current word as "+word);
     if (connected) {
 	client.write( 
 	    mrcp_message (
 		'SEGMENT',
 		[{'name': 'Reference-Transcript',
 		  'value' : word }],
-		audio ), 
+		null ), 
 	    function (err) {
 		if (err) {
 		    console.log(err.stack);
@@ -130,15 +189,34 @@ function define_word(word) {
     }
 }
 
-
-
-function send_audio( audio) {
+function define_speaker(speaker) {
+    console.log("===================================> Trying to define current speaker as "+speaker);
     if (connected) {
 	client.write( 
 	    mrcp_message (
-		'AUDIO',
-		[],
-		audio ), 
+		'SET-PARAMS',
+		[{'name': 'Voice-Name',
+		  'value' : speaker }],
+		null ), 
+	    function (err) {
+		if (err) {
+		    console.log(err.stack);
+		}			    
+	    });	
+    }
+    else {
+	console.log("Recogniser not connected yet!");
+    }
+}
+
+function start_recog() {
+    if (connected) {
+	client.write( 
+	    mrcp_message (
+		'RECOGNIZE',
+		[{'name': 'Confidence-Threshold',
+		  'value' : '0.0' }],
+		null ), 
 	    function (err) {
 		if (err) {
 		    console.log(err.stack);
@@ -151,16 +229,137 @@ function send_audio( audio) {
 }
 
 
-
-
-
-
-
-
-function get_result() {
-    return recog_result;
+function finish_audio() {
+    //console.log("###################### FINISHING AUDIO!!!! ###########################");
+    send_audio(new Buffer(0));
+    
+/*      if (connected) {
+      client.write(
+      mrcp_message (
+      'AUDIO',
+      [],
+      null ), 
+      function (err) {
+      if (err) {
+      console.log(err.stack);
+      }			    
+      });	
+      }
+      else {
+      console.log("Recogniser not connected yet!");
+      }*/
 }
 
+
+
+function send_audio(data) {
+    //console.log("??????????????? Pushing to audio queue! ");
+    audio_queue.push(data);
+    //console.log("??????????????? Audio queue: ");
+    //var ct=0;
+    //audio_queue.forEach( function(item) {
+    //    console.log("["+(ct++)+ "] Buffer length "+ item.length);
+    //});
+    process_audio_queue();
+}
+
+
+eventEmitter.on('audio_packages_sent', function () { 
+    process_audio_queue();
+});
+
+
+function process_audio_queue() {
+    if (connected) {
+	if (sending_data == false) {
+	    //console.log("???????????? Data sending free to use!");
+	    if (audio_queue.length > 0) {
+		//console.log("???????????? Packets in queue, let's send");
+		sending_data=true;
+		data_package = audio_queue.splice(0,1)
+		send_audio_packets(data_package[0], 0, 0);
+	    }
+	}
+	//else {
+	//    console.log("???????????? Data sending in progress, let's wait");
+	//}
+    }
+    else {
+	console.log("Recogniser not connected yet!");
+    }
+}
+
+
+
+
+function send_audio_packets (data, i, queue_index) {
+
+    //console.log((data.length-i));
+    //console.log(recog_conf.packet_size);
+    //console.log(Math.min( (data.length-i), recog_conf.packet_size));  
+
+    this_packet_size = Math.min( (data.length-i), recog_conf.packet_size);    
+    
+    //if (this_packet_size == 0) {
+    //	console.log("###################### SIZE 0 PACKET SENT!!!! ###########################");
+    //}
+
+
+    client.write(
+	mrcp_message (
+	    'AUDIO',
+	    [{ 'name' : 'Content-Length',
+	       'value' : this_packet_size }],
+	    data.slice(i, i+this_packet_size ) ),
+	function (err) {
+	    //console.log("??????????????? wrote to msg queue, now what?");
+	    if (err) {
+		//console.log("??????????????? we have error 1: "+err);
+		console.log(err.stack);
+	    } 
+	    else 
+	    {
+		//console.log("????? Writing to recogniser "+i+"-"+(i+this_packet_size));
+		//console.log("data.slice("+i+", "+(i+this_packet_size)+")" );
+		//console.log(data.slice(i, i+this_packet_size));
+		if (this_packet_size > 0) {
+		     client.write( data.slice(i, i+this_packet_size), function (err) {
+			 //console.log("??????????????? wrote to msg queue, now what?");
+
+			 if (err) {		
+			     //console.log("??????????????? we have error 2: "+err);
+			     console.log(err.stack);
+			 } else {
+			     if (i+this_packet_size < data.length)
+			     {
+				 i+=recog_conf.packet_size;
+				 //console.log("??????????????? calling myself at "+i);
+				 setTimeout (function() {
+				     send_audio_packets(data,i, queue_index);
+				 }, recog_conf.pause_between_packets);
+			     }
+			     else {
+				 //console.log("??????????????? !"+(i+this_packet_size)+" < "+data.length);
+				 sending_data = false;
+				 eventEmitter.emit('audio_packages_sent');
+			     }
+			 }
+		     });
+		}
+		else {
+		    // Done! This was the last packet.
+		    sending_data = false;		    
+		}
+	    }
+	});	 
+}
+
+
+
+function reset_recogniser() {
+    state = 'start_recog';
+    start_recog();
+}
 
 
 module.exports = { 
@@ -168,6 +367,10 @@ module.exports = {
     init : init,  
     define_grammar: define_grammar,
     define_word : define_word,
+    define_speaker : define_speaker,
+    start_recog : start_recog,
     send_audio : send_audio,
-    get_result : get_result
+    finish_audio : finish_audio,
+    reset_recogniser: reset_recogniser
+    //get_result : get_result
 };
--- server_script.js
+++ server_script.js
@@ -43,11 +43,16 @@ var audioconf = {
     'lsforder'               : 15,
     'lsflength'              : 16, // Should be order +1
     'mceporder'              : 12,
-    'mceplength'             : 13 
+    'mceplength'             : 13,
+    'debug_mfcc'             : false,
+    'debug_f0'               : false,
+    'debug_lsf'              : false
 };
 
 var recogconf = {
-    'grammar' : 'words.conf'
+    'grammar' : 'words.conf',
+    'packet_size' : 2048,
+    'pause_between_packets' : 20
 }
 
 
@@ -75,69 +80,43 @@ http.createServer(function (req, res) {
     //util.log('Request recieved: \nmethod: ' + req.method + '\nurl: ' + req.url) // this line logs just the method and url
 
     user = req.headers['x-siak-user'];
+    packetnr = req.headers['x-siak-packetnr'];
 
     if (user) {
-	
-	if (typeof userdata[user] == 'undefined') {
-	    
-	    userdata[user] = {};
-	    
-	    userdata[user].recogniser = require('./recogniser_client');
-	    userdata[user].recogniser.init(recogconf);
+	if (packetnr == -1) {
+	    init_userdata(user);
+	} 
 
-	    //userdata[user].audiopackets=[];
-	    userdata[user].packetset=[];
-	    
-	    userdata[user].lastpacketnr=-2;
-	    userdata[user].analysedpackets=0;
-
-	    userdata[user].features=[];
-	    userdata[user].audiobinarydata = new Buffer( audioconf.max_utterance_length_s * audioconf.fs * audioconf.datatype_length );
-	    userdata[user].audiobinarydata.fill(0);
-
-	    userdata[user].featuredata = new Buffer( Math.ceil(audioconf.max_utterance_length_s * audioconf.fs / audioconf.frame_step_samples * audioconf.feature_dim) );
-	    userdata[user].featuredata.fill(0);
-
-	    userdata[user].bufferend=0;
-	    userdata[user].sent_to_analysis = 0;
-
-	    userdata[user].chunkeddata = new Buffer( audioconf.max_packet_length_s * audioconf.fs * audioconf.datatype_length );
-	    userdata[user].featuresdone = [0,0,0];
-	    userdata[user].analysed = 0;
-
-	    userdata[user].currentword=null;
-	}
-
-
-	packetnr = req.headers['x-siak-packetnr'];
-
-	if (packetnr == 0 ) {
-	    userdata[user].currentword=req.headers['X-siak-current-word'];	
-	    userdata[user].recogniser.define_word(userdata[user].currentword);
-	}
-
-	finalpacket = req.headers['x-siak-final-packet'];
-	arraystart = parseInt(req.headers['x-siak-packet-arraystart']);
-	arrayend = parseInt(req.headers['x-siak-packet-arrayend']);
-	arraylength = parseInt(req.headers['x-siak-packet-arraylength']);
-	
-	debugout("Packet nr "+packetnr + " finalpacket " +finalpacket+" arraystart " + arraystart+ " arrayend " + arrayend);
-
-	userdata[user].thisstart=arraystart;
-	
-	if (finalpacket == "true") {
-	    lastpacket=packetnr;
-	    userdata[user].lastpacketnr=packetnr;
-	}
 	else {
-	    lastpacket = false;
-	}
-	
-	userdata[user].chunkeddata.fill(0);
+	    if (packetnr == 0 ) {
 
-	var startcounter=0;
-	var chunkct=0;
-	var postdata = '';
+		//userdata[user].currentword = req.headers['x-siak-current-word'];	
+		//userdata[user].recogniser.define_word( userdata[user].currentword );
+	    }
+	    
+	    finalpacket = req.headers['x-siak-final-packet'];
+	    arraystart = parseInt(req.headers['x-siak-packet-arraystart']);
+	    arrayend = parseInt(req.headers['x-siak-packet-arrayend']);
+	    arraylength = parseInt(req.headers['x-siak-packet-arraylength']);
+	    
+	    //debugout("Packet nr "+packetnr + " finalpacket " +finalpacket+" arraystart " + arraystart+ " arrayend " + arrayend);
+	    
+	    userdata[user].thisstart=arraystart;
+	    
+	    if (finalpacket == "true") {
+		lastpacket=packetnr;
+		userdata[user].lastpacketnr=packetnr;
+	    }
+	    else {
+		lastpacket = false;
+	    }
+	    
+	    userdata[user].chunkeddata.fill(0);
+	    
+	    var startcounter=0;
+	    var chunkct=0;
+	    var postdata = '';
+	}
 
 	req.on('data', function (chunk, encoding) {
 	    
@@ -146,29 +125,69 @@ http.createServer(function (req, res) {
    	});
 	
 	req.on('end', function () {
-	    userdata[user].packetset[ packetnr ] = 1;
-	    
-	    // For debug:
-	    if (debug) {
-		fs.writeFile("upload_data/debug/"+user+"_packet_"+packetnr, postdata);
+	    if (packetnr == -1) {
+		userdata[user].recogniser = require('./recogniser_client');
+		userdata[user].recogniser.init(recogconf, res, function(res) {
+		    res.end( JSON.stringify({msg: "<br>Recognition server initialised!"}) );
+		});				
 	    }
+	    else {
+		userdata[user].packetset[ packetnr ] = 1;
+	    
+		// For debug:
+		if (debug) { fs.writeFile("upload_data/debug/"+user+"_packet_"+packetnr, postdata); }
 	    	
-	    decodedchunks=new Buffer(postdata, 'base64');
-
-	    decodedchunks.copy( // src buffer
-		userdata[user].audiobinarydata, // targetbuffer
-		arraystart*audioconf.datatype_length, // targetstart
-		0, // sourcestart
-		decodedchunks.length); //source-length	 	    
-
-	    userdata[user].bufferend = Math.max( (arrayend)*audioconf.datatype_length, userdata[user].bufferend);
-
-	    processDataChunks(user, res, packetnr);		
+		decodedchunks=new Buffer(postdata, 'base64');
+		
+		decodedchunks.copy( // src buffer
+		    userdata[user].audiobinarydata, // targetbuffer
+		    arraystart*audioconf.datatype_length, // targetstart
+		    0, // sourcestart
+		    decodedchunks.length); //source-length	 	    
+		
+		userdata[user].bufferend = Math.max( (arrayend)*audioconf.datatype_length, userdata[user].bufferend);		
+		processDataChunks(user, res, packetnr);		
+	    }
 	});
     }
 }).listen(process.env.PORT || 8001);
 
+
 debugout('Server running on port '+ (process.env.PORT || 8001) );
+
+
+
+
+function init_userdata(user) {
+    if (typeof userdata[user] == 'undefined') {
+	
+	userdata[user] = {};
+	
+	//userdata[user].audiopackets=[];
+	userdata[user].packetset=[];
+	
+	userdata[user].lastpacketnr=-2;
+	userdata[user].analysedpackets=0;
+	
+	userdata[user].features=[];
+	userdata[user].audiobinarydata = new Buffer( audioconf.max_utterance_length_s * audioconf.fs * audioconf.datatype_length );
+	userdata[user].audiobinarydata.fill(0);
+	
+	userdata[user].featuredata = new Buffer( Math.ceil(audioconf.max_utterance_length_s * audioconf.fs / audioconf.frame_step_samples * audioconf.feature_dim) );
+	userdata[user].featuredata.fill(0);
+	
+	userdata[user].bufferend=0;
+	userdata[user].sent_to_analysis = 0;
+	
+	userdata[user].chunkeddata = new Buffer( audioconf.max_packet_length_s * audioconf.fs * audioconf.datatype_length );
+	userdata[user].featuresdone = [0,0,0];
+	userdata[user].analysed = 0;
+	
+	userdata[user].currentword=null;
+
+    }
+};
+
 
 
 function processDataChunks(user, res, packetnr) {
@@ -178,24 +197,32 @@ function processDataChunks(user, res, packetnr) {
 	// We do not know yet what is the last packet.
 	// Do stuff with packet and acknowledge with message:
 
-	res.end( JSON.stringify({msg: "<br>Processing packet "+packetnr+" ---"}) );
-	process.emit('sendAudioForAnalysis', user);
+	if (packetnr > -1) {
+	    res.end( JSON.stringify({msg: "<br>Processing packet "+packetnr+" ---"}) );
+	    process.emit('sendAudioForAnalysis', user);
+	}
     }
     else {
 	// We know what the last packet is;
 	// If it's not this one, we might have received it earlier and
 	// it's waiting to for the data to be complete:
-	if (packetnr != userdata[user].lastpacketnr) {
-	    // Emit an event to the listener holding back the reply to the last packet:
 
+	if (packetnr != userdata[user].lastpacketnr) {
+	    
+	    // We know this is not the last packet, so reply to it quickly:
 	    res.end( JSON.stringify({msg: "<br>Processing packet "+packetnr+" --- Last packet is "+userdata[user].lastpacketnr }) );
+	    
+
+	    // Emit an event to the listener holding back the reply to the last packet
+	    // (This is just for the odd possibility that packages would arrive
+	    // in a strange order.)
 	    process.emit('lastPacketCheck', user);
+
+
 	}
 	else {
 	    // We're dealing with the last packet; Let's see if we have received all packets:
 	    // Count packets, see if it matches our nubmer:
-
-	    debugout("+++ Storing the last packet in userdata["+user+"].lastPacketRes");
 
 	    userdata[user].lastPacketRes=res;
 	    process.emit('lastPacketCheck', user);
@@ -205,18 +232,29 @@ function processDataChunks(user, res, packetnr) {
 
 
 process.on('lastPacketCheck', //checkLastPacket);
+	   function(user) 
+	   {
+	       checkLastPacket(user);
+	   });
 
-function (user) {
+
+function checkLastPacket(user) {
     
     var chunkcount = -1;    
     userdata[user].packetset.forEach( function(element, index, array) {
 	chunkcount++;
     });
     
-    if (chunkcount == userdata[user].lastpacketnr ) {	
 
+    // Check if we have all the packets in already:
+    if (chunkcount == userdata[user].lastpacketnr ) {	
+	
 	syncAudioAnalysis(user);
-	    
+	
+	// Send a null packet to recogniser as sign of finishing:
+	console.log('==((===))=== Finishing audio:');
+	userdata[user].recogniser.finish_audio();
+	
 	debugout('Upload complete, should be finishing processing, but instead just saving the data and clearing');
 	
 	if (debug) {
@@ -234,7 +272,8 @@ function (user) {
 	debugout( "Checking for last: "+chunkcount+" == "+userdata[user].lastpacketnr );
 	process.emit('sendAudioForAnalysis', user);
     }
-});
+}
+
 
 function clearUpload(user) {
     debugout('Clearing upload data');
@@ -263,18 +302,17 @@ function clearUpload(user) {
 
 
 
-process.on('sendAudioForAnalysis', // sendDataToAcousticAnalysis);
-
-function (user) {
-    syncAudioAnalysis(user);
-});
+process.on('sendAudioForAnalysis', // sendDataToAcousticAnalysis);	   
+	   function (user) {
+	       syncAudioAnalysis(user);
+	   });
 
 function syncAudioAnalysis(user) {
 
     // Available packets:
     // userdata[user].packetset
 
-    debugout("sendDataToAcousticAnalysis: user "+user);
+    //debugout("sendDataToAcousticAnalysis: user "+user);
 
 
     // Which part of the buffer can be already analysed?
@@ -286,7 +324,8 @@ function syncAudioAnalysis(user) {
     if (range_start > audioconf.frame_length_samples - audioconf.frame_step_samples) 
     {
 	// Take into account the frame overlap:
-	range_start -= Math.floor(audioconf.frame_length_samples/audioconf.frame_step_samples)*audioconf.frame_step_samples; 
+	range_start -= Math.floor( audioconf.frame_length_samples / audioconf.frame_step_samples) * audioconf.frame_step_samples; 
+
     }
 
     range_end= (userdata[user].bufferend - (userdata[user].bufferend % audioconf.frame_step_samples));
@@ -296,11 +335,12 @@ function syncAudioAnalysis(user) {
     result_range_length = range_length - Math.floor(audioconf.frame_length_samples/audioconf.frame_step_samples)*audioconf.frame_step_samples;
 
 
-    console.log("Sending to analysis: "+ range_start +"..."+range_end +" and receiving "+ range_start + "..." + (range_start+result_range_length) );
+    //console.log("Sending to analysis: "+ range_start +"..."+range_end +" and receiving "+ range_start + "..." + (range_start+result_range_length) );
+    //console.log(' inputbuffer length: ' + userdata[user].audiobinarydata.slice(range_start,range_length).length);
+    //console.log('outputbuffer length: ' + userdata[user].featuredata.slice( range_start, result_range_length ).length);
 
-    console.log(' inputbuffer length: ' + userdata[user].audiobinarydata.slice(range_start,range_length).length);
-    console.log('outputbuffer length: ' + userdata[user].featuredata.slice( range_start, result_range_length ).length);
 
+    // Send data to the DNN feature extractor:
 
     var audio_analyser = require('./audio_analyser');
 
@@ -312,21 +352,14 @@ function syncAudioAnalysis(user) {
 
 
     // Send data to the recogniser process:
-    userdata[user].recogniser.send_audio(
-	userdata[user].audiobinarydata.slice(
+
+    send_to_recogniser(user, userdata[user].audiobinarydata.slice(
 	    userdata[user].sent_to_analysis,
 	    range_end ) );
     
     userdata[user].sent_to_analysis=range_end;
 
 
-    
-
-
-
-    //fileBuffer = new Buffer(userdata[user].audiopackets[i], "base64");
-    //fs.writeFileSync('./upload_data/'+i, fileBuffer);
-    
     if (debug) {
 	fs.writeFileSync('upload_data/debug/test_feature_'+user, userdata[user].featuredata);
     }
@@ -334,21 +367,46 @@ function syncAudioAnalysis(user) {
 
 
 
+function send_to_recogniser(user, floatdata) {
+
+    console.log("************ Sending to recogniser "+floatdata.length+" floats of data!");
+
+    //userdata[user].recogniser.send_audio(floatdata);
+
+    pcmdata = new Buffer(floatdata.length/2);
+
+    for (var i = 0; i < pcmdata.length; i+=2) {
+	pcmdata.writeInt16LE( (floatdata.readFloatLE(i*2) * 32767), i );
+    }
+    userdata[user].recogniser.send_audio(pcmdata);
+  
+
+//    // This was for debug purposes:  
+//    if (Buffer.isBuffer(userdata[user].pcm)) {
+//	userdata[user].pcm = Buffer.concat([userdata[user].pcm, pcmdata]);
+//    } else {
+//	userdata[user].pcm = pcmdata;
+//    }
+
+}
+
+
+
 
 process.on('mfccDone', function (user, packetcode) { 
-    console.log( '   // Received event: // mfcc '+packetcode+' done for '+user); 
+    //console.log( '   // Received event: // mfcc '+packetcode+' done for '+user); 
     userdata[user].featuresdone[0] = Math.max( userdata[user].featuresdone[0], packetcode );    
     check_feature_progress(user);
 });
 
 process.on('lsfDone', function (user, packetcode) { 
-    console.log( '   // Received event: // lsf '+packetcode+' done for '+user); 
+    //console.log( '   // Received event: // lsf '+packetcode+' done for '+user); 
     userdata[user].featuresdone[1] = Math.max( userdata[user].featuresdone[1], packetcode );    
     check_feature_progress(user);
 });
 
 process.on('logF0Done', function (user, packetcode) { 
-    console.log( '   // Received event: // logF0 batch '+packetcode+' done for '+user); 
+    //console.log( '   // Received event: // logF0 batch '+packetcode+' done for '+user); 
     userdata[user].featuresdone[2] = Math.max( userdata[user].featuresdone[2], packetcode );    
     check_feature_progress(user);
 });
@@ -360,29 +418,35 @@ function check_feature_progress(user) {
 
 	// Analyse up to the new max point.
 	var maxpoint =  Math.min.apply (Math, userdata[user].featuresdone);
-	debugout("*** Got data up to "+ maxpoint + ", lastpacket="+userdata[user].lastpacketnr);
+	//debugout("*** Got data up to "+ maxpoint + ", lastpacket="+userdata[user].lastpacketnr);
 	
 	userdata[user].analysed = maxpoint;
-    
+	
 	if (maxpoint => userdata[user].bufferend) {
-
+	    
 	    if (userdata[user].lastpacketnr > 0) {
 		debugout("*** Last packet, here goes!" );
 		
+		if (debug) { 
+		    if (Buffer.isBuffer(userdata[user].pcm)) {
+			fs.writeFileSync('upload_data/debug/pcm_'+user, userdata[user].pcm);
+			
+		    }
+		}
+		    
+				
 		// Send a random number back, as we don't know of any better.
 		userdata[user].lastPacketRes.end( JSON.stringify({score: 5.0*Math.random(), msg: "<br>All " + userdata[user].lastpacketnr +" packets received!"}) );
 		
-		
-	
 		clearUpload(user)
 	    }
 	}
-
+	
 	//userdata[user].featuresdone = [false,false,false];
     }
     else {
 	debugout("*** Waiting for more features ("+ userdata[user].featuresdone.toString() +" / "+  Math.min.apply (Math, userdata[user].featuresdone)   +")")
     }
-
+    
 }
 
