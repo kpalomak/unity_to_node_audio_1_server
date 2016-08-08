14c452827bef5b2ea7d153902ec1f16cf14e828e
siak <kalle.palomaki@aalto.fi>
2016-08-08 21:43:20 +0300

    added audio based scoring debug script

--- server/audio_handling/run_word_cross_likelihood.sh
+++ server/audio_handling/run_word_cross_likelihood.sh
@@ -9,7 +9,7 @@ port="5554"
 corpus_dir="/home/siak/siak/"
 #awk '{gsub(" +","_");print $0}'
 wav_name="/home/siak/siak-server/process_audio/S10_chop/S10_pronunciation_1.wav"
-
+path_speaker="../upload_data/from_game/foo/" 
 
 #variable=`date | awk '{gsub(" +","_");print $0}' |awk '{gsub(":+","_");print $0}'`
 variable="a"
@@ -27,4 +27,4 @@ export PYTHONPATH="${repository_dir}/pyfrontend/src"
 
 #"${script_dir}/test-network-wordlist_katja_too_model_shrink.py" "${host}" "${port}" "${corpus_dir}" "log_${variable}.txt"
 
-"${script_dir}/word_cross_likelihood_score.py" "too" "$wav_name" "log_${variable}.txt"
+"${script_dir}/word_cross_likelihood_score.py" "too" "$wav_name" "$path_speaker" "log_${variable}.txt"
--- server/audio_handling/shell_script_aligner_quick.sh
+++ server/audio_handling/shell_script_aligner_quick.sh
@@ -8,22 +8,36 @@
 # $4 labelinput
 # $5 model
 # $6 segmentoutput
+# $7 flag_use_adaptation
+# $8 adaptation_matrix_name
 
 #cp $3 "/l/data/siak-server-devel/server/upload_data/from_game/${1}_`date +"%Y-%m-%d-%H-%M-%S"`.wav"
 
-echo $1 ", " $2 ", "  $3 ", " $4 ", " $5 ", " $6
+echo $1 ", " $2 ", "  $3 ", " $4 ", " $5 ", " $6 ", " $7 ", " $8 , ", " $9
+(>&2 echo "kekkonen")
 
 printf "__\n" > $4
-egrep "^$1\(1.0\)" $2 | head -n 1 | cut -d " " -f 2- | tr '[ ]' '[\n]' | iconv -f utf-8 -t ISO-8859-15 >> $4
+#egrep "^$1\(1.0\)" $2 | head -n 1 | cut -d " " -f 2- | tr '[ ]' '[\n]' | iconv -f utf-8 -t ISO-8859-15 >> $4
+./audio_handling/get_model.py $1 $2 >> $4
 printf "__\n" >> $4
 
 #egrep "^owl\(1.0\)" /home/siak/models/clean-am/train_utf-8.lex | head -n 1 | cut -d " " -f 2- | tr '[ ]' '[\n]' > $4
 
 model=$5 #"/home/backend/models-clean-am/siak_clean_a"
-cfg=$7
+#model="/home/siak/models/clean-am/siak_clean_b"
+
 printf  $4
 
-echo "audio=$3 transcript=$4 alignment=$6 speaker=S1" | \
-    align -i 2 --swins=100000 -b $model -c $cfg -r /dev/stdin -S ./S1_50words.spkc
+#check if adaptation flag is on
+(>&2 echo "$8.spkc")
 
+if [ "$7" = "1" ] && [ -f "$8.spkc" ]
+    then
+	echo "audio=$3 transcript=$4 alignment=$6 speaker=S" | \
+    		align -i 2 --swins=100000 -b $model -c $9 -r /dev/stdin -S $8.spkc
 
+else 
+	echo "audio=$3 transcript=$4 alignment=$6" | \
+	    align -i 2 --swins=100000 -b $model -c $9 -r /dev/stdin
+	(>&2 echo "aligning without adaptation")
+fi
